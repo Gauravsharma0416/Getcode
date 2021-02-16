fae6b877431f6df8a41eb993ca7f2442c5d13f6f
Gaurav <Gauravsharma0416@gmail.com>
2021-02-16 16:21:10 +0530

    myproject

---
+++ login.php
@@ -0,0 +1,8 @@
+<?php include("header.php") ?>
+
+<center>
+    <h2 >Login</h2>
+</center>
+
+
+<?php include("footer.php") ?>
---
+++ services.php
@@ -0,0 +1,8 @@
+<?php include("header.php") ?>
+
+<center>
+    <h2 >Services</h2>
+</center>
+
+
+<?php include("footer.php") ?>
